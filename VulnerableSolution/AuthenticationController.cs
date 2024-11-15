a47444e2b5aadaedcfed71f01f66a45bedc1fd5e
cosminprocesio <cosmin.nastase@procesio.com>
2024-11-15 16:15:13 +0200

    Added authentication controller. Added Token Generation method.

---
+++ VulnerableSolution/AuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using VulnerableSolution.Models;
+
+namespace VulnerableSolution
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult LogIn(
+            [FromHeader] string userName,
+            [FromHeader] string password)
+        {
+            string connectionString = "YourConnectionStringHere";
+            UserModel currentUser = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Username, Email, Password FROM Users WHERE Username = '" + userName + "'";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    currentUser = new UserModel()
+                    {
+                        UserName = reader.GetString(reader.GetOrdinal("Username")),
+                        Email = reader.GetString(reader.GetOrdinal("Email")),
+                        Password = reader.GetString(reader.GetOrdinal("Password"))
+                    };
+                }
+                // Process data...
+            }
+            if(currentUser == null)
+            {
+                return BadRequest("Invalid username");
+            }
+
+            if (!currentUser.Password.Equals(password))
+            {
+                return BadRequest("Invalid password");
+            }
+
+            var token = Token.Generate(userName);
+            return Ok(new { token });
+        }
+    }
+}
---
+++ VulnerableSolution/Token.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace VulnerableSolution
+{
+    public class Token
+    {
+        private static readonly string _secret = "Cmy$uper$ecretOver99999";
+
+        public static string Generate(string username)
+        {
+            string timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+
+            string token = $"{username}:{timestamp}:{_secret}";
+
+            byte[] tokenBytes = Encoding.UTF8.GetBytes(token);
+            string base64Token = Convert.ToBase64String(tokenBytes);
+
+            return base64Token;
+        }
+    }
+}
