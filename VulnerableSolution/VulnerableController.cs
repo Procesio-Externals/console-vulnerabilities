1e4632294285671d607309f1a38f889b91a7df62
corina.rusu <corina.rusu@procesio.com>
2024-11-18 15:56:39 +0200

    adding encryption endpoint

--- VulnerableSolution/VulnerableController.cs
+++ VulnerableSolution/VulnerableController.cs
@@ -1,6 +1,7 @@
 // Microsoft.AspNetCore.Mvc.Core 2.2.5 is deprecated
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using VulnerableSolution;
 
 namespace WebAPI.Controllers
 {
@@ -10,7 +11,9 @@ namespace WebAPI.Controllers
     {
         // SQL Injection Vulnerability: User input is directly concatenated into the SQL command
         [HttpGet("get-user")]
-        public IActionResult GetUser(string username)
+        public IActionResult GetUser(
+            [FromHeader] string token, //username should be taken from token not body payload
+            string username)
         {
             string connectionString = "YourConnectionStringHere";
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -22,6 +25,13 @@ namespace WebAPI.Controllers
                 // Process data...
             }
             return Ok();
+        }
+
+        // library used considers default hash algorithm as insecure and outdated
+        [HttpGet("encrypt")]
+        public IActionResult Encrypt(string text)
+        {
+            return Ok(Encryption.Encrypt(text));
         }
     }
 }
