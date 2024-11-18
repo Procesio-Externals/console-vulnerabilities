49d07c9bdd841a9f7ea77d7a37e6c377d473a190
corina.rusu <corina.rusu@procesio.com>
2024-11-18 16:49:31 +0200

    added port leak example

---
+++ VulnerableSolution/MemoryLeaks/PortLeak.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace VulnerableSolution.MemoryLeaks
+{
+    //Keeping ports open unnecessarily in your app can lead to resource leaks, security vulnerabilities, and system instability.
+    //E.g. Failing to release a network socket can cause app to run out of resources, potentially crashing it or preventing new connections.
+    public class PortLeak
+    {
+        public void KeepPortsAlive()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                // Open a socket and leave it without closing
+                var listener = new TcpListener(IPAddress.Loopback, 0);
+                listener.Start();
+
+                Console.WriteLine($"Port {((IPEndPoint)listener.LocalEndpoint).Port} is open.");
+            }
+        }
+    }
+}
