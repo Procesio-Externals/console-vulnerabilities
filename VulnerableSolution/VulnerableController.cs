b95aab391569a08bac575c31c564a7cedd785f69
Alexandru Gal <alexandru.gal@procesio.com>
2025-07-22 10:03:40 +0300

    update comment

--- VulnerableSolution/VulnerableController.cs
+++ VulnerableSolution/VulnerableController.cs
@@ -19,6 +19,7 @@ public class VulnerableController : ControllerBase
     }
 
     // SQL Injection Vulnerability: User input is directly concatenated into the SQL command
+	// console trigger.
     [HttpGet("get-user")]
     public IActionResult GetUser(
         [FromHeader] string token, //username should be taken from token not body payload
